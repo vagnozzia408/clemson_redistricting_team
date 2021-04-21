ae71dd11c20db5e281d456679ee865cc808cc675
Blake Splitter <bsplitt@clemson.edu>
2021-04-21 11:12:15 -0400

    Updates to ArcGIS Maps

--- SC_Redistricting_Updated/SA_for_counties.py
+++ SC_Redistricting_Updated/SA_for_counties.py
@@ -6,7 +6,7 @@ Created on Thu Apr 15 16:52:28 2021
 """
 
 
-import arcpy,numpy
+import arcpy,numpy,random,math
 
 def FieldCheck(in_table):
     lstFields = arcpy.ListFields(in_table)
@@ -29,7 +29,9 @@ def DeviationFromIdealPop(sumpop):
     for i in range(SPlength):
         absdev[i] = abs(sumpop[i]-idealpop)
     deviation = sum(absdev)
+    deviation = round(deviation)
     return(deviation)
+        
     
     
 # Set environment settings
@@ -37,6 +39,7 @@ path = r"C:\Users\blake\Documents\Clemson Materials\Research\Saltzman Research\c
 arcpy.env.workspace = path
 
 in_table="tl_2020_45_county20"
+
 
 #Returns DistField "Dist_Assgn" and creates the field if it's not already there
 DistField = FieldCheck(in_table)
@@ -53,7 +56,7 @@ arcpy.AddMessage("There are {0} rows in {1}".format(rowcount,in_table))
 
 #Finds sum of each district population
 sumpop=[]
-sumpop = [0 for i in range(7)]
+sumpop = [0]*7
 with arcpy.da.SearchCursor(in_table, ['SUM_Popula',DistField]) as cursor:
     for row in cursor:
         #i = district number minus 1, since district numbers range from 1 to 7
@@ -62,29 +65,56 @@ with arcpy.da.SearchCursor(in_table, ['SUM_Popula',DistField]) as cursor:
         sumpop[i] = row[0] + sumpop[i]
 arcpy.AddMessage("sumpop is {0}".format(sumpop))            
 
-T = 100
+T = 123000
 count = 0
-while T>0.1 and count <10:
-    #arcpy.AddMessage("count = {}. About to add 1.".format(count))
+MaxIter =10000
+deviation =[0]*(MaxIter+1)
+deviation[0] = 10000000
+while T>0.1 and count <MaxIter:
+    arcpy.AddMessage("count = {}. About to add 1.".format(count))
     count = count+1
     #NEED TO FIX LINE BELOW. 46 SHOULD BE ROWCOUNT
     randshape = numpy.random.randint(0,46)
     with arcpy.da.UpdateCursor(in_table, ['FID', DistField, 'SUM_Popula'],"FID = {0}".format(randshape)) as cursor:
         for row in cursor:
             currdist = int(row[1])
-            
+            distpop = row[2]
             #Ensures that the district assignment is actually changed
             while currdist == row[1]:
                 row[1] = numpy.random.randint(1,8)
             newdist = row[1]
             #subtracts population from district with fewer people
-            sumpop[currdist-1] = sumpop[currdist-1] - row[2]
-            #adds population to distrinct that just gained people
-            sumpop[newdist-1] = sumpop[newdist-1] + row[2]
-            arcpy.AddMessage("sumpop is {0}".format(sumpop)) 
+            sumpop[currdist-1] = sumpop[currdist-1] - distpop
+            #adds population to district that just gained people
+            sumpop[newdist-1] = sumpop[newdist-1] + distpop
+            #arcpy.AddMessage("sumpop is {0}".format(sumpop)) 
             cursor.updateRow(row)   
-    deviation = DeviationFromIdealPop(sumpop)
-    deviation = round(deviation)
-    arcpy.AddMessage("absolute deviation is {0}".format(deviation))    
+    deviation[count] = DeviationFromIdealPop(sumpop)
+    #arcpy.AddMessage("absolute deviation is {0}".format(deviation[count]))    
+    DeltaE = deviation[count] - deviation[count-1]
+    #arcpy.AddMessage("DeltaE = {0}. T = {1}".format(DeltaE,T))
+    if DeltaE <0:
+        T = T*.999
+        continue
+    else :
+        rand = random.uniform(0,1)
+        try: 
+            p = 1/math.exp(DeltaE/T)
+        except OverflowError:
+            p = 0
+        #arcpy.AddMessage("p = {0}. rand = {1}".format(p,rand))
+        if rand>=p:
+            T = T*.999
+            continue
+        else: #undoes the district changes previously made. 
+            count = count-1
+            sumpop[currdist-1] = sumpop[currdist-1] + distpop
+            sumpop[newdist-1] = sumpop[newdist-1] - distpop
+            with arcpy.da.UpdateCursor(in_table, ['FID', DistField, 'SUM_Popula'],"FID = {0}".format(randshape)) as cursor:
+                for row in cursor:
+                    row[1] = currdist
+                    cursor.updateRow(row)
+
+                
     
             
